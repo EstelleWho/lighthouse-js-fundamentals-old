dfbdce7f9b0302964cb3eacb04dd7a336fdc38ea
EstelleWho <122816900+EstelleWho@users.noreply.github.com>
2023-02-12 22:59:30 -0500

    Add files via upload

---
+++ loopyRange.js
@@ -0,0 +1,27 @@
+function range(start, end, step) {
+    if (start === undefined || end === undefined || step === undefined || start > end || step < 0)
+        return [];
+    else {
+        var range = [];
+        for (var i = start; i <= end; i += step) {
+            range.push(i);
+        }
+        return range;
+    }
+}//need to double check spaces
+
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+
+function range(start, end, step) {
+    const array = [];
+    if (start > end || step <= 0 || start === undefined || end === undefined || step === undefined) {
+        return array;
+    } else for (let i = start; i <= end; i = i + step) {
+        array.push(i);
+    }
+    return array;
+}
+
